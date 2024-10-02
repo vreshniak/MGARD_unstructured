d3e5859867fc05e2bdb7ddc9ddeefce050ebdf36
Viktor Reshniak <reshniakv@ornl.gov>
2024-10-02 10:49:16 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+module load PrgEnv-gnu
+module load cray-mpich
+
 adios2_ROOT="/ccs/home/reshniakv/libs_frontier/adios"
 export CMAKE_PREFIX_PATH=$adios2_ROOT:$CMAKE_PREFIX_PATH
 
@@ -6,3 +9,4 @@ export CMAKE_PREFIX_PATH=$mgard_ROOT:$CMAKE_PREFIX_PATH
 
 cmake -S . -B build
 cmake --build build
+

ee17928a969c24c9e37973df275bd317ada868a6
vreshniak <reshniakv@ornl.gov>
2024-10-02 12:30:52 -0400

    update compress_mgard

--- src/compress_mgard.cpp
+++ src/compress_mgard.cpp
@@ -24,6 +24,26 @@ using BYTE = unsigned char;
 
 int main(int argc, char *argv[])
 {
+	///////////////////////////////////////////////////////////////////////////
+	// MPI
+
+	int rank, nranks;
+#if ADIOS2_USE_MPI
+	int provided;
+
+	// MPI_THREAD_MULTIPLE is only required if you enable the SST MPI_DP
+	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nranks);
+#else
+	rank = 0;
+	nranks = 1;
+#endif
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// config
+
 	// read config file
 	if (argc<4){
 		std::cerr << "Config JSON file is required as input" << std::endl;
@@ -40,7 +60,11 @@ int main(int argc, char *argv[])
 
 
 	// setup ADIOS readers/writers
+#if ADIOS2_USE_MPI
+	adios2::ADIOS adios(MPI_COMM_WORLD);
+#else
 	adios2::ADIOS adios;
+#endif
 	adios2::IO reader_io = adios.DeclareIO("BPReader");
 	adios2::IO writer_io = adios.DeclareIO("BPWriter");
 	reader_io.SetEngine("BPFile");
@@ -48,12 +72,48 @@ int main(int argc, char *argv[])
 	adios2::Engine bpReader = reader_io.Open(argv[2], adios2::Mode::Read);
 	adios2::Engine bpWriter = writer_io.Open(argv[3], adios2::Mode::Write);
 
-	adios2::IO writer2_io = adios.DeclareIO("BPWriter2");
-	writer2_io.SetEngine("BPFile");
-	adios2::Engine bpWriter2 = writer2_io.Open(argv[4], adios2::Mode::Write);
+	// adios2::IO writer2_io = adios.DeclareIO("BPWriter2");
+	// writer2_io.SetEngine("BPFile");
+	// adios2::Engine bpWriter2 = writer2_io.Open(argv[4], adios2::Mode::Write);
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// compression config
+
+	const DTYPE s = config["s"];
+	const DTYPE rel_tol = config["rel_tol"];
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// define output ADIOS variables
+
+
+	adios2::Variable<int64_t> out_adios_connectivity = writer_io.DefineVariable<int64_t>(connectivity_name, {}, {}, {adios2::UnknownDim});
+
+	std::vector<adios2::Variable<double>> out_adios_coos(coo_names.size());
+	for (int i=0; i<coo_names.size(); i++)
+		out_adios_coos[i] = writer_io.DefineVariable<double>(coo_names[i], {}, {}, {adios2::UnknownDim});
+
+	std::vector<adios2::Variable<BYTE>> out_adios_vars(var_names.size());
+	for (int i=0; i<var_names.size(); i++)
+		out_adios_vars[i] = writer_io.DefineVariable<BYTE>(var_names[i], {}, {}, {adios2::UnknownDim});
+
+
+	///////////////////////////////////////////////////////////////////////////
+
+
+	double total_time = 0;
+
+
+	// time stepping
+	while (true){
 
 
 	adios2::StepStatus read_status = bpReader.BeginStep(adios2::StepMode::Read, -1.0f);
+	size_t step = bpReader.CurrentStep();
+	if (step>config["max_step"] or read_status!=adios2::StepStatus::OK)
+		break;
+	bpWriter.BeginStep();
 
 
 	///////////////////////////////////////////////////////////////////////////
@@ -71,31 +131,7 @@ int main(int argc, char *argv[])
 
 
 	///////////////////////////////////////////////////////////////////////////
-	// define output ADIOS variables
-
-	// adios2::Variable<int64_t> out_adios_connectivity = writer_io.DefineVariable<int64_t>(connectivity_name, {}, {}, {adios2::UnknownDim});
-
-	// std::vector<adios2::Variable<double>> out_adios_coos(coo_names.size());
-	// for (int i=0; i<coo_names.size(); i++)
-	// 	out_adios_coos[i] = writer_io.DefineVariable<double>(coo_names[i], {}, {}, {adios2::UnknownDim});
-
-	std::vector<adios2::Variable<BYTE>> out_adios_vars(var_names.size());
-	for (int i=0; i<var_names.size(); i++)
-		out_adios_vars[i] = writer_io.DefineVariable<BYTE>(var_names[i], {}, {}, {adios2::UnknownDim});
-
-
-	std::vector<adios2::Variable<double>> out_adios_vars2(var_names.size());
-	for (int i=0; i<var_names.size(); i++)
-		out_adios_vars2[i] = writer2_io.DefineVariable<double>(var_names[i], {}, {}, {adios2::UnknownDim});
-
-
-	///////////////////////////////////////////////////////////////////////////
 	// compress variables
-
-
-	const DTYPE s = config["s"];
-	const DTYPE rel_tol = config["rel_tol"];
-
 
 	std::vector<double> original_size(var_names.size(), 0.0);
 	std::vector<double> compressed_size(var_names.size(), 0.0);
@@ -106,7 +142,8 @@ int main(int argc, char *argv[])
 	size_t nblocks = blocks.size();
 	int block_id = 0;
 	for (auto &block : blocks){
-		std::cout << "blockID = " << block.BlockID << " out of " << nblocks << std::endl;
+		auto start = std::chrono::high_resolution_clock::now();
+		std::cout << "step = " << step << ", blockID = " << block.BlockID << "/" << nblocks;
 
 		// allocate memory for reading variables
 		std::vector<int64_t> ElementConnectivity;
@@ -135,39 +172,39 @@ int main(int argc, char *argv[])
 		size_t N = coos[0].size();
 
 		// Coordinate array
-		std::array<std::vector<DTYPE>, 1> mgard_coos;
-		std::vector<DTYPE> &mgard_x = mgard_coos.at(0);
-		mgard_x.reserve(N);
-		mgard_x.push_back(0.0);
-		double cumul_dst = 0;
-		for (int i=1; i<N; i++){
-			double dst = 0;
-			for (auto &coo : coos)
-				dst += (coo[i]-coo[i-1]) * (coo[i]-coo[i-1]);
-			cumul_dst += std::sqrt(dst) + 1.e-10;
-			// cumul_dst += 1;
-			mgard_x.push_back(cumul_dst);
-		}
-		for (int i=0; i<N; i++){
-			// mgard_x[i] /= mgard_x[N-1];
-			mgard_x[i] = float(i)/(N-1);
-		}
+		// std::array<std::vector<DTYPE>, 1> mgard_coos;
+		// std::vector<DTYPE> &mgard_x = mgard_coos.at(0);
+		// mgard_x.reserve(N);
+		// mgard_x.push_back(0.0);
+		// double cumul_dst = 0;
+		// for (int i=1; i<N; i++){
+		// 	double dst = 0;
+		// 	for (auto &coo : coos)
+		// 		dst += (coo[i]-coo[i-1]) * (coo[i]-coo[i-1]);
+		// 	cumul_dst += std::sqrt(dst) + 1.e-10;
+		// 	// cumul_dst += 1;
+		// 	mgard_x.push_back(cumul_dst);
+		// }
+		// for (int i=0; i<N; i++){
+		// 	// mgard_x[i] /= mgard_x[N-1];
+		// 	mgard_x[i] = float(i)/(N-1);
+		// }
 
 		// wrap the information about the mesh into an `mgard::TensorMeshHierarchy`
-		const mgard::TensorMeshHierarchy<1, DTYPE> hierarchy({N}, mgard_coos);
-		// const mgard::TensorMeshHierarchy<1, DTYPE> hierarchy({N});
+		// const mgard::TensorMeshHierarchy<1, DTYPE> hierarchy({N}, mgard_coos);
+		const mgard::TensorMeshHierarchy<1, DTYPE> hierarchy({N});
 
 
 		//////////////////////////////////////////////////////////////////////////////////
 		// save compressed variables
 
-		// out_adios_connectivity.SetSelection(adios2::Box<adios2::Dims>({}, {ElementConnectivity.size()}));
-		// bpWriter.Put<int64_t>(out_adios_connectivity, ElementConnectivity.data(), adios2::Mode::Sync);
+		out_adios_connectivity.SetSelection(adios2::Box<adios2::Dims>({}, {ElementConnectivity.size()}));
+		bpWriter.Put<int64_t>(out_adios_connectivity, ElementConnectivity.data(), adios2::Mode::Sync);
 
-		// for (int i=0; i<coo_names.size(); i++){
-		// 	out_adios_coos[i].SetSelection(adios2::Box<adios2::Dims>({}, {coos[i].size()}));
-		// 	bpWriter.Put<DTYPE>(out_adios_coos[i], coos[i].data(), adios2::Mode::Sync);
-		// }
+		for (int i=0; i<coo_names.size(); i++){
+			out_adios_coos[i].SetSelection(adios2::Box<adios2::Dims>({}, {coos[i].size()}));
+			bpWriter.Put<DTYPE>(out_adios_coos[i], coos[i].data(), adios2::Mode::Sync);
+		}
 
 		for (int i=0; i<var_names.size(); i++){
 			// find absolute tolerance
@@ -268,48 +305,64 @@ int main(int argc, char *argv[])
 			bpWriter.Put<BYTE>(out_adios_vars[i], (BYTE*)compressed.data(), adios2::Mode::Sync);
 
 
-			const mgard::DecompressedDataset<1, DTYPE> decompressed = mgard::decompress(compressed);
-			out_adios_vars2[i].SetSelection(adios2::Box<adios2::Dims>({}, {N}));
-			bpWriter2.Put<DTYPE>(out_adios_vars2[i], (DTYPE*)decompressed.data(), adios2::Mode::Sync);
+			// const mgard::DecompressedDataset<1, DTYPE> decompressed = mgard::decompress(compressed);
+			// out_adios_vars2[i].SetSelection(adios2::Box<adios2::Dims>({}, {N}));
+			// bpWriter2.Put<DTYPE>(out_adios_vars2[i], (DTYPE*)decompressed.data(), adios2::Mode::Sync);
 
 		}
 		bpWriter.PerformPuts();
-		bpWriter2.PerformPuts();
+		// bpWriter2.PerformPuts();
 
 
-	if (config["max_block_id"]>=0 and block_id++>=config["max_block_id"]) break;
+		///////////////////////////////////////////////////////////////
 
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+		total_time += (double)duration.count() / 1e6;
+
+		std::cout << std::setprecision(3) << ", time = " << (double)duration.count() / 1e6 << " sec" << std::endl;
+
+
+		///////////////////////////////////////////////////////////////
+
+
+		if (config["max_block_id"]>=0 and block_id++>=config["max_block_id"]) break;
 	}
 
 
-	// print compression ratios
-	for (int i=0; i<var_names.size(); i++)
-		std::cout << var_names[i] << " ";
-	std::cout << std::endl;
-	for (int i=0; i<var_names.size(); i++){
-		std::cout << rel_errors[i] << " ";
-	}
-	std::cout << std::endl;
-	for (int i=0; i<var_names.size(); i++){
-		std::cout << original_size[i] << " ";
-	}
-	std::cout << std::accumulate(original_size.begin(), original_size.end(), decltype(original_size)::value_type(0));
-	std::cout << std::endl;
-	for (int i=0; i<var_names.size(); i++){
-		std::cout << compressed_size[i] << " ";
-	}
-	std::cout << std::accumulate(compressed_size.begin(), compressed_size.end(), decltype(compressed_size)::value_type(0));
-	std::cout << std::endl;
-	for (int i=0; i<var_names.size(); i++){
-		std::cout << original_size[i] / compressed_size[i] << " ";
-	}
-	std::cout << std::accumulate(original_size.begin(), original_size.end(), decltype(original_size)::value_type(0)) / std::accumulate(compressed_size.begin(), compressed_size.end(), decltype(compressed_size)::value_type(0)) << " ";
-	std::cout << std::endl;
+	// // print compression ratios
+	// for (int i=0; i<var_names.size(); i++) std::cout << var_names[i]     << " "; std::cout << std::endl;
+	// for (int i=0; i<var_names.size(); i++) std::cout << rel_errors[i]    << " "; std::cout << std::endl;
+	// for (int i=0; i<var_names.size(); i++) std::cout << original_size[i] << " ";
+	// std::cout << std::accumulate(original_size.begin(), original_size.end(), decltype(original_size)::value_type(0));
+	// std::cout << std::endl;
+	// for (int i=0; i<var_names.size(); i++) std::cout << compressed_size[i] << " ";
+	// std::cout << std::accumulate(compressed_size.begin(), compressed_size.end(), decltype(compressed_size)::value_type(0));
+	// std::cout << std::endl;
+	// for (int i=0; i<var_names.size(); i++) std::cout << original_size[i] / compressed_size[i] << " ";
+	// std::cout << std::accumulate(original_size.begin(), original_size.end(), decltype(original_size)::value_type(0)) / std::accumulate(compressed_size.begin(), compressed_size.end(), decltype(compressed_size)::value_type(0)) << " ";
+	// std::cout << std::endl;
 
 
-	bpReader.EndStep(); // end logical step
+	// bpReader.EndStep(); // end logical step
+	// bpReader.Close();  // close engine
+	// bpWriter.Close();  // close engine
+	// bpWriter2.Close();  // close engine
+
+	bpReader.EndStep();	// end logical step
+	bpWriter.EndStep();	// end logical step
+	}
+
+
+	std::cout << "\nTotal time = " << std::setprecision(3) << total_time << std::endl;
+
+
 	bpReader.Close();  // close engine
 	bpWriter.Close();  // close engine
-	bpWriter2.Close();  // close engine
 
+#if ADIOS2_USE_MPI
+	MPI_Finalize();
+#endif
+	return 0;
 }

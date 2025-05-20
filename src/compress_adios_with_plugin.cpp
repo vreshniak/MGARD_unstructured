b97bac5c77a04b7f2f2840c2372c67ea81f52360
vreshniak <reshniakv@ornl.gov>
2025-05-20 10:42:27 -0400

    minor fixes

--- src/compress_adios_with_plugin.cpp
+++ src/compress_adios_with_plugin.cpp
@@ -45,10 +45,14 @@ int main(int argc, char *argv[])
 
 	// read config file
 	if (argc<4){
-		std::cerr << "Config JSON file is required as input" << std::endl;
+		std::cerr << "Usage: compress_adios_plugin config.json orig.bp compressed.bp, compress_adios_plugin got " << argc-1 << " parameters" << std::endl;
 		return -1;
 	}
-	std::ifstream f(argv[1]);
+	std::string config_file_name = argv[1];
+	std::string input_file_name  = argv[2];
+	std::string output_file_name = argv[3];
+
+	std::ifstream f(config_file_name);
 	json config = json::parse(f);
 
 
@@ -66,8 +70,8 @@ int main(int argc, char *argv[])
 	adios2::IO writer_io = adios.DeclareIO("BPWriter");
 	reader_io.SetEngine("BPFile");
 	writer_io.SetEngine("BPFile");
-	adios2::Engine bpReader = reader_io.Open(argv[2], adios2::Mode::Read);
-	adios2::Engine bpWriter = writer_io.Open(argv[3], adios2::Mode::Write);
+	adios2::Engine bpReader = reader_io.Open(input_file_name,  adios2::Mode::Read);
+	adios2::Engine bpWriter = writer_io.Open(output_file_name, adios2::Mode::Write);
 
 
 	///////////////////////////////////////////////////////////////////////////
@@ -98,96 +102,95 @@ int main(int argc, char *argv[])
 	// time stepping
 	while (true){
 
-
-	adios2::StepStatus read_status = bpReader.BeginStep(adios2::StepMode::Read, -1.0f);
-	size_t step = bpReader.CurrentStep();
-	if (step>config["max_step"] or read_status!=adios2::StepStatus::OK)
-		break;
-	bpWriter.BeginStep();
-
-
-	///////////////////////////////////////////////////////////////////////////
-	// define input ADIOS variables
-
-	std::vector<adios2::Variable<double>> adios_vars(var_names.size());
-	std::vector<double> vars_min(var_names.size());
-	std::vector<double> vars_max(var_names.size());
-	for (int i=0; i<var_names.size(); i++){
-		adios_vars[i] = reader_io.InquireVariable<double>(var_names[i]);
-		vars_min[i] = adios_vars[i].Min();
-		vars_max[i] = adios_vars[i].Max();
-	}
+		adios2::StepStatus read_status = bpReader.BeginStep(adios2::StepMode::Read, -1.0f);
+		size_t step = bpReader.CurrentStep();
+		if (step>config["max_step"] or read_status!=adios2::StepStatus::OK)
+			break;
+		bpWriter.BeginStep();
 
 
-	///////////////////////////////////////////////////////////////////////////
-	// compress variables
+		///////////////////////////////////////////////////////////////////////////
+		// define input ADIOS variables
 
-
-	// number of blocks/subdomains
-	auto blocks = bpReader.BlocksInfo(adios_vars[0], 0);
-	size_t nblocks = blocks.size();
-	int block_id = 0;
-	for (auto &block : blocks){
-		auto start = std::chrono::high_resolution_clock::now();
-		std::cout << "step = " << step << ", blockID = " << block.BlockID << "/" << nblocks << std::endl;
-
-		// allocate memory for reading variables
-		std::vector<std::vector<double>> vars(var_names.size());
-
-		// set block/subregion for the variables
-		// adios_connectivity.SetBlockSelection(block.BlockID);
-		for (auto &var : adios_vars) var.SetBlockSelection(block.BlockID);
-
-		// read variables
-		for (int i=0; i<var_names.size(); i++) bpReader.Get(adios_vars[i], vars[i], adios2::Mode::Sync);
-		bpReader.PerformGets();
-
-
-		//////////////////////////////////////////////////////////////////////////////////
-		// save compressed variables
-
+		std::vector<adios2::Variable<double>> adios_vars(var_names.size());
+		std::vector<double> vars_min(var_names.size());
+		std::vector<double> vars_max(var_names.size());
 		for (int i=0; i<var_names.size(); i++){
-			// find absolute tolerance
-			DTYPE mag_v = 0;
-			for (size_t k=0; k<vars[i].size(); k++)
-				mag_v += vars[i][k] * vars[i][k] / vars[i].size();
-			DTYPE L2_norm = std::sqrt(mag_v);
-			DTYPE abs_tol = rel_tol * L2_norm;
-
-			adios2::Params params;
-			params["PluginName"] = "serialize_operator";
-			params["PluginLibrary"] = "CompressMGARDSerializeOperator";
-			params["meshfile"] = "/lustre/orion/cfd164/proj-shared/reshniakv/order.bp"; //argv[2];
-			params["blockid"] = std::to_string(block_id);
-			params["accuracy"] = std::to_string(abs_tol);
-			out_adios_vars[i].AddOperation("plugin", params);
-			// // out_adios_vars[i].AddOperation(op, {{"accuracy", std::to_string(abs_tol)}, {"mode", "ABS"}});
-
-			out_adios_vars[i].SetSelection(adios2::Box<adios2::Dims>({}, {vars[i].size()}));
-			bpWriter.Put<DTYPE>(out_adios_vars[i], vars[i].data(), adios2::Mode::Sync);
-
-			out_adios_vars[i].RemoveOperations();
+			adios_vars[i] = reader_io.InquireVariable<double>(var_names[i]);
+			vars_min[i] = adios_vars[i].Min();
+			vars_max[i] = adios_vars[i].Max();
 		}
-		bpWriter.PerformPuts();
 
 
-		///////////////////////////////////////////////////////////////
-
-		// auto end = std::chrono::high_resolution_clock::now();
-		// auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-		// total_time += (double)duration.count() / 1e6;
-
-		// std::cout << std::setprecision(3) << ", time = " << (double)duration.count() / 1e6 << " sec" << std::endl;
+		///////////////////////////////////////////////////////////////////////////
+		// compress variables
 
 
-		///////////////////////////////////////////////////////////////
+		// number of blocks/subdomains
+		auto blocks = bpReader.BlocksInfo(adios_vars[0], 0);
+		size_t nblocks = blocks.size();
+		int block_id = 0;
+		for (auto &block : blocks){
+			auto start = std::chrono::high_resolution_clock::now();
+			std::cout << "step = " << step << ", blockID = " << block.BlockID << "/" << nblocks << std::endl;
 
-		if (config["max_block_id"]>=0 and block_id++>=config["max_block_id"]) break;
-	}
+			// allocate memory for reading variables
+			std::vector<std::vector<double>> vars(var_names.size());
 
-	bpReader.EndStep();	// end logical step
-	bpWriter.EndStep();	// end logical step
+			// set block/subregion for the variables
+			// adios_connectivity.SetBlockSelection(block.BlockID);
+			for (auto &var : adios_vars) var.SetBlockSelection(block.BlockID);
+
+			// read variables
+			for (int i=0; i<var_names.size(); i++) bpReader.Get(adios_vars[i], vars[i], adios2::Mode::Sync);
+			bpReader.PerformGets();
+
+
+			//////////////////////////////////////////////////////////////////////////////////
+			// save compressed variables
+
+			for (int i=0; i<var_names.size(); i++){
+				// find absolute tolerance
+				DTYPE mag_v = 0;
+				for (size_t k=0; k<vars[i].size(); k++)
+					mag_v += vars[i][k] * vars[i][k] / vars[i].size();
+				DTYPE L2_norm = std::sqrt(mag_v);
+				DTYPE abs_tol = rel_tol * L2_norm;
+
+				adios2::Params params;
+				params["PluginName"] = "serialize_operator";
+				params["PluginLibrary"] = "CompressMGARDSerializeOperator";
+				params["meshfile"] = "/lustre/orion/cfd164/proj-shared/reshniakv/order.bp"; //argv[2];
+				params["blockid"] = std::to_string(block_id);
+				params["accuracy"] = std::to_string(abs_tol);
+				out_adios_vars[i].AddOperation("plugin", params);
+				// // out_adios_vars[i].AddOperation(op, {{"accuracy", std::to_string(abs_tol)}, {"mode", "ABS"}});
+
+				out_adios_vars[i].SetSelection(adios2::Box<adios2::Dims>({}, {vars[i].size()}));
+				bpWriter.Put<DTYPE>(out_adios_vars[i], vars[i].data(), adios2::Mode::Sync);
+
+				out_adios_vars[i].RemoveOperations();
+			}
+			bpWriter.PerformPuts();
+
+
+			///////////////////////////////////////////////////////////////
+
+			// auto end = std::chrono::high_resolution_clock::now();
+			// auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+			// total_time += (double)duration.count() / 1e6;
+
+			// std::cout << std::setprecision(3) << ", time = " << (double)duration.count() / 1e6 << " sec" << std::endl;
+
+
+			///////////////////////////////////////////////////////////////
+
+			if (config["max_block_id"]>=0 and block_id++>=config["max_block_id"]) break;
+		}
+
+		bpReader.EndStep();	// end logical step
+		bpWriter.EndStep();	// end logical step
 	}
 
 

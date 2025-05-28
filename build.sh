4346d257302c73c3c761e54d4836410371d81154
vreshniak <reshniakv@ornl.gov>
2025-05-28 10:17:36 -0400

    update build system

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 module load PrgEnv-gnu
 module load cray-mpich
+# module load ums/default
+# module load ums002/default
+# module load sz3/3.1.7
+# module load zstd/1.5.0
 
 adios2_ROOT="/ccs/home/reshniakv/libs_frontier/adios"
 export CMAKE_PREFIX_PATH=$adios2_ROOT:$CMAKE_PREFIX_PATH
+
+zfp_ROOT="/ccs/home/reshniakv/libs_frontier/zfp"
+export CMAKE_PREFIX_PATH=$zfp_ROOT:$CMAKE_PREFIX_PATH
+
+sz3_ROOT="/ccs/home/reshniakv/libs_frontier/sz3"
+export CMAKE_PREFIX_PATH=$sz3_ROOT:$CMAKE_PREFIX_PATH
 
 mgard_ROOT="/ccs/home/reshniakv/MGARD/install-hip-frontier"
 export CMAKE_PREFIX_PATH=$mgard_ROOT:$CMAKE_PREFIX_PATH
